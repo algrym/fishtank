2ba9cb70fda9ef7a4dbce239b4c3275f0a672bc4
A. J. Wright <ajw@toadking.org>
2023-05-16 20:28:10 -0400

    Objectified mobile, randomized creation and direction

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,57 @@
 use std::io;
 use std::thread;
 use std::time::Duration;
+use rand::Rng;
 
-use console::{style, Term};
+use console::{Emoji, Term};
+
+struct Mobile {
+    x: u16,
+    y: u16,
+    delta_x: i16,
+    delta_y: i16,
+}
+
+impl Mobile {
+    fn update(&mut self, height: u16, width: u16) {
+        if self.x >= width - 1 { self.delta_x = -1 }
+        if self.y >= height - 1 { self.delta_y = -1 }
+
+        if self.x == 0 { self.delta_x = 1 }
+        if self.y == 0 { self.delta_y = 1 }
+
+        self.x = self.x.wrapping_add_signed(self.delta_x);
+        self.y = self.y.wrapping_add_signed(self.delta_y);
+    }
+
+    fn render(&mut self, term: &Term) {
+        term.move_cursor_to(usize::from(self.x), usize::from(self.y));
+        term.write_str(&format!("{}", Emoji("🦀", "@")));
+    }
+}
 
 fn render() -> io::Result<()> {
     let term = Term::stdout();
     let (height, width) = term.size();
 
+    let mut rng = rand::thread_rng();
+
     term.set_title("Fishtank");
     term.hide_cursor()?;
     term.clear_screen()?;
 
-    let mut x: u16 = 0;
-    let mut y: u16 = 0;
-    let mut c: u8 = 0;
-
-    let mut delta_x: i16 = 1;
-    let mut delta_y: i16 = 1;
+    let mut mob = Mobile {
+        x: rng.gen_range(0..width),
+        y: rng.gen_range(0..height),
+        delta_x: if rng.gen_bool(0.5) { 1 } else { -1 },
+        delta_y: if rng.gen_bool(0.5) { 1 } else { -1 },
+        };
 
     loop {
-        // Update locations
-        if x >= width - 1 { delta_x = -1 }
-        if y >= height - 1 { delta_y = -1 }
-
-        if x == 0 { delta_x = 1 }
-        if y == 0 { delta_y = 1 }
-
-        if c == u8::MAX { c = 0 } else { c += 1 }
-
-        x = x.wrapping_add_signed(delta_x);
-        y = y.wrapping_add_signed(delta_y);
-
-        // Render element
-        term.move_cursor_to(usize::from(x), usize::from(y))?;
-        term.write_str(&format!("{}", style("@").color256(c)))?;
-        thread::sleep(Duration::from_millis(20));
-        term.clear_chars(1)?;
+        mob.update(height, width);
+        mob.render(&term);
+        thread::sleep(Duration::from_millis(50));
+        term.clear_screen()?;
     }
 }
 
