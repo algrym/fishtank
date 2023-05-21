f0a804987eac7e9120a9169440c3776215dda9f4
A. J. Wright <ajw@toadking.org>
2023-05-20 20:45:53 -0400

    Initial set up for Bevy 2D

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::ScalingMode};
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin, SystemInformationDiagnosticsPlugin};
+use rand::Rng;
 
 #[derive(Component)]
 struct MobileFish {
@@ -11,9 +13,18 @@ struct Location {
     y: i32,
 }
 
-fn add_fish(mut commands: Commands) {
+fn spawn_fish(mut commands: Commands) {
+    let mut rng = rand::thread_rng();
     for i in 0..9 {
-        commands.spawn((MobileFish { name: i.to_string() }, Location { x: i, y: i }));
+        commands.spawn((MobileFish { name: i.to_string() }, Location { x: rng.gen_range(0..9), y: rng.gen_range(0..9) }));
+        commands.spawn(SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0., 0.47, 1.),
+                custom_size: Some(Vec2::new(1., 1.)),
+                ..default()
+            },
+            ..default()
+        });
     }
 }
 
@@ -21,11 +32,34 @@ fn show_fish(query: Query<(&MobileFish, &Location)>) {
     for (fish, loc) in &query {
         println!("Fish {} at {},{}", fish.name, loc.x, loc.y);
     }
+    println!();
+}
+
+fn setup(mut commands: Commands) {
+    let mut camera_bundle = Camera2dBundle::default();
+    camera_bundle.projection.scaling_mode = ScalingMode::FixedVertical(10.);
+    commands.spawn(camera_bundle);
 }
 
 fn main() {
     App::new()
-        .add_startup_system(add_fish)
+        .insert_resource(ClearColor(Color::rgb(0.1, 0.1, 0.8)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                fit_canvas_to_parent: true, // fill the entire browser window
+                prevent_default_event_handling: false, // don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
+                title: "Fishtank! - ajw@ajw.io".to_string(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_startup_systems((setup, spawn_fish))
         .add_system(show_fish)
+
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        // SystemInformationDiagnostics don't work if you're dynamic linking. :|
+        .add_plugin(SystemInformationDiagnosticsPlugin::default())
+
         .run();
 }
