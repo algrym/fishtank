e143e0a224c55fb12a42922991f6acd30e9d5293
A. J. Wright <ajw@toadking.org>
2023-05-27 08:46:10 -0400

    Initial setup done: drawing a ball

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use bevy::{
-    diagnostic::{FrameTimeDiagnosticsPlugin,
-                 LogDiagnosticsPlugin,
-                 SystemInformationDiagnosticsPlugin},
+    diagnostic::{
+        FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin, SystemInformationDiagnosticsPlugin,
+    },
     prelude::*,
-    render::camera::ScalingMode,
+    sprite::*,
+    window::WindowResolution,
 };
 use rand::Rng;
 
 const _TIMESTEP_1_PER_SECOND: f32 = 1.0;
+
+const WINDOW_WIDTH: f32 = 1024.0;
+const WINDOW_HEIGHT: f32 = 720.0;
+
+// Remember, in Bevy's coordinate system the origin is at the center of the screen
+const WINDOW_BOTTOM_Y: f32 = WINDOW_HEIGHT / -2.0;
+const WINDOW_LEFT_X: f32 = WINDOW_WIDTH / -2.0;
 
 #[derive(Component)]
 struct MobileFish {
@@ -20,15 +28,27 @@ struct Location {
     y: i32,
 }
 
-fn spawn_fish(mut commands: Commands) {
+fn spawn_fish(mut commands: Commands,
+              mut meshes: ResMut<Assets<Mesh>>,
+              mut materials: ResMut<Assets<ColorMaterial>>) {
     let mut rng = rand::thread_rng();
     for i in 0..9 {
-        commands.spawn((MobileFish { name: i.to_string() }, Location { x: rng.gen_range(0..9), y: rng.gen_range(0..9) }));
-        commands.spawn(SpriteBundle {
-            sprite: Sprite {
-                color: Color::rgb(0., 0.47, 1.),
-                custom_size: Some(Vec2::new(1., 1.)),
-                ..default()
+        commands.spawn((
+            MobileFish {
+                name: i.to_string(),
+            },
+            Location {
+                x: rng.gen_range(0..9),
+                y: rng.gen_range(0..9),
+            },
+        ));
+        commands.spawn(MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::default().into()).into(),
+            material: materials.add(ColorMaterial::from(Color::LIME_GREEN)),
+            transform: Transform {
+                translation: Vec3::new(WINDOW_LEFT_X + 100.0, WINDOW_BOTTOM_Y + 30.0, 0.0),
+                scale: Vec3::new(30.0, 30.0, 1.0),
+                ..Default::default()
             },
             ..default()
         });
@@ -43,16 +63,19 @@ fn show_fish(query: Query<(&MobileFish, &Location)>) {
 
 fn setup(mut commands: Commands) {
     let mut camera_bundle = Camera2dBundle::default();
-    camera_bundle.projection.scaling_mode = ScalingMode::FixedVertical(10.);
+    // camera_bundle.projection.scaling_mode = ScalingMode::FixedVertical(20.);
     commands.spawn(camera_bundle);
 }
 
 fn main() {
     App::new()
-        .insert_resource(ClearColor(Color::rgb(0.1, 0.1, 0.8))) // background color
-        .add_plugins(DefaultPlugins.set(WindowPlugin { // set up window
+        .insert_resource(ClearColor(Color::BLUE)) // background color
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            // set up window
             primary_window: Some(Window {
-                fit_canvas_to_parent: true, // fill the entire browser window
+                fit_canvas_to_parent: true,            // fill the entire browser window
+                resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
+                resizable: false,
                 prevent_default_event_handling: false, // don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
                 title: "Fishtank! - ajw@ajw.io".to_string(),
                 ..default()
@@ -61,11 +84,9 @@ fn main() {
         }))
         .add_startup_systems((setup, spawn_fish))
         .add_system(show_fish) // TODO: these run too frequently
-
         .add_plugin(LogDiagnosticsPlugin::default())
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
         // SystemInformationDiagnostics don't work if you're dynamic linking. :|
         .add_plugin(SystemInformationDiagnosticsPlugin::default())
-
         .run();
 }
