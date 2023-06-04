e93f3a1cb00ef232943075af835b2c1798b78fcf
A. J. Wright <ajw@toadking.org>
2023-06-03 20:05:10 -0400

    Bouncing sprites with randomly variable speed.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,9 @@ use bevy::{
     diagnostic::{
         FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin, SystemInformationDiagnosticsPlugin,
     },
-    prelude::*,
-    sprite::*,
-    time::common_conditions::on_fixed_timer,
-    utils::Duration,
-    window::WindowResolution,
     math::f32::Vec3,
+    prelude::*,
+    window::WindowResolution,
 };
 use rand::Rng;
 
@@ -28,66 +25,81 @@ struct MobileFish {
 }
 
 #[derive(Component)]
-struct Location {
-    x: i32,
-    y: i32,
+struct Direction {
+    horizontal_speed: f32,
+    vertical_speed: f32,
 }
 
-fn spawn_fish(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-) {
+fn spawn_fish(mut commands: Commands, asset_server: Res<AssetServer>) {
     let mut rng = rand::thread_rng();
     for i in 0..9 {
+        info!("spawn_fish {}", i);
         commands.spawn((
             MobileFish {
-                name: i.to_string(),
+                name: i.to_string()
             },
-            Location {
-                x: rng.gen_range(WINDOW_LEFT_X..WINDOW_RIGHT_X),
-                y: rng.gen_range(WINDOW_BOTTOM_Y..WINDOW_TOP_Y),
+            Direction {
+                horizontal_speed: rng.gen_range(-10.0..10.0),
+                vertical_speed: rng.gen_range(-10.0..10.0),
             },
-        ));
-        commands.spawn(SpriteBundle {
-            transform: Transform {
-                translation: Vec3::new(0.0, -220.0, 0.1),
-                scale: Vec3::new(0.1, 0.1, 1.0),
+            SpriteBundle {
+                transform: Transform {
+                    translation: Vec3::new(rng.gen_range(WINDOW_LEFT_X..WINDOW_RIGHT_X) as f32,
+                                           rng.gen_range(WINDOW_BOTTOM_Y..WINDOW_TOP_Y) as f32,
+                                           0.1),
+                    scale: Vec3::new(0.1, 0.1, 1.0),
+                    ..Default::default()
+                },
+                texture: asset_server.load("red.png"),
                 ..Default::default()
             },
-            texture: asset_server.load("red.png"),
-            ..Default::default()
-        });
-    }
-}
-
-fn print_position_system(query: Query<&Transform>) {
-    for transform in &query {
-        println!("position: {:?}", transform.translation);
-    }
-}
-
-fn show_fish(query: Query<(&MobileFish, &Location)>) {
-    for (fish, loc) in &query {
-        info!("🐟{}({},{}) ", fish.name, loc.x, loc.y);
+        ));
     }
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
+    commands.spawn(SpriteBundle { // Background
         texture: asset_server.load("dark.png"),
         transform: Transform::from_scale(Vec3::new(1.0, 1.0, -1.0)),
         ..Default::default()
     });
 }
 
-fn update_fish() {
-    info!("update_fish");
+fn update_fish(mut query: Query<(&MobileFish, &mut Direction, &mut Transform)>) {
+    let mut rng = rand::thread_rng();
+
+    for (fish, mut fish_direction, mut fish_transform) in query.iter_mut() {
+        info!("update_fish 🐟{}({}) h{} v{}", fish.name, fish_transform.translation, fish_direction.horizontal_speed, fish_direction.vertical_speed);
+        fish_direction.horizontal_speed += rng.gen_range(-0.1..0.1);
+        fish_direction.vertical_speed += rng.gen_range(-0.1..0.1);
+    }
+}
+
+fn move_fish(mut query: Query<(&MobileFish, &mut Direction, &mut Transform)>) {
+    for (fish, mut fish_direction, mut fish_transform) in query.iter_mut() {
+        debug!("move_fish 🐟{}({}) h{} v{}", fish.name, fish_transform.translation, fish_direction.horizontal_speed, fish_direction.vertical_speed);
+        fish_transform.translation.x += fish_direction.horizontal_speed;
+        fish_transform.translation.y += fish_direction.vertical_speed;
+
+        if (fish_transform.translation.x > WINDOW_RIGHT_X as f32) ||
+            (fish_transform.translation.x < WINDOW_LEFT_X as f32)
+        {
+            fish_direction.horizontal_speed *= -1.0;
+        }
+
+        if (fish_transform.translation.y > WINDOW_TOP_Y as f32) ||
+            (fish_transform.translation.y < WINDOW_BOTTOM_Y as f32)
+        {
+            fish_direction.vertical_speed *= -1.0;
+        }
+    }
 }
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::MIDNIGHT_BLUE)) // background color
+
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             // set up window
             primary_window: Some(Window {
@@ -100,16 +112,20 @@ fn main() {
             }),
             ..default()
         }))
+
         .add_startup_systems((setup, spawn_fish))
-        .add_plugin(LogDiagnosticsPlugin::default())
-        .add_plugin(FrameTimeDiagnosticsPlugin::default())
+
         // SystemInformationDiagnostics don't work if you're dynamic linking. :|
         .add_plugin(SystemInformationDiagnosticsPlugin::default())
-        .add_system(print_position_system)
-        .add_system(
-            show_fish
-                .in_schedule(CoreSchedule::FixedUpdate)
-                .run_if(on_fixed_timer(Duration::from_secs(TIMESTEP_1_PER_SECOND))),
-        )
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
+
+        .add_system(move_fish)
+        .add_system(update_fish)
+        //.add_system(
+        //    update_fish
+        //        .in_schedule(CoreSchedule::FixedUpdate)
+        //        .run_if(on_fixed_timer(Duration::from_secs(TIMESTEP_1_PER_SECOND))),
+        //)
         .run();
 }
