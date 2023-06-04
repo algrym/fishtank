4446cfcb5f5e7a639ef8fd77369e2e320e7f79fc
A. J. Wright <ajw@toadking.org>
2023-06-03 21:24:45 -0400

    Load fish sprites from spritesheet and point them in correct direction.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use bevy::{
     },
     math::f32::Vec3,
     prelude::*,
+    time::common_conditions::on_fixed_timer,
+    utils::Duration,
     window::WindowResolution,
 };
 use rand::Rng;
@@ -19,6 +21,13 @@ const WINDOW_LEFT_X: i32 = WINDOW_WIDTH / -2;
 const WINDOW_TOP_Y: i32 = WINDOW_HEIGHT / 2;
 const WINDOW_RIGHT_X: i32 = WINDOW_WIDTH / 2;
 
+const SPRITE_SHEET_CELL_WIDTH: f32 = 64.0;
+const SPRITE_SHEET_COLUMNS: usize = 17;
+const SPRITE_SHEET_ROWS: usize = 7;
+const SPRITE_SHEET_MAX: usize = SPRITE_SHEET_COLUMNS * SPRITE_SHEET_ROWS;
+const SPRITE_SHEET_FISH_INDEX_LOW: usize = 68;
+const SPRITE_SHEET_FISH_INDEX_HIGH: usize = SPRITE_SHEET_FISH_INDEX_LOW + 10;
+
 #[derive(Component)]
 struct MobileFish {
     name: String,
@@ -30,36 +39,52 @@ struct Direction {
     vertical_speed: f32,
 }
 
-fn spawn_fish(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn spawn_fish(mut commands: Commands,
+              asset_server: Res<AssetServer>,
+              mut texture_atlases: ResMut<Assets<TextureAtlas>>)
+{
+    let texture_handle = asset_server.load("fishTileSheet.png");
+    let texture_atlas =
+        TextureAtlas::from_grid(texture_handle,
+                                Vec2::new(SPRITE_SHEET_CELL_WIDTH, SPRITE_SHEET_CELL_WIDTH),
+                                SPRITE_SHEET_COLUMNS, SPRITE_SHEET_ROWS, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
     let mut rng = rand::thread_rng();
-    for i in 0..9 {
+    for i in SPRITE_SHEET_FISH_INDEX_LOW..SPRITE_SHEET_FISH_INDEX_HIGH {
         info!("spawn_fish {}", i);
         commands.spawn((
             MobileFish {
-                name: i.to_string()
+                name: i.to_string(),
             },
             Direction {
-                horizontal_speed: rng.gen_range(-10.0..10.0),
-                vertical_speed: rng.gen_range(-10.0..10.0),
+                horizontal_speed: rng.gen_range(-5.0..5.0),
+                vertical_speed: rng.gen_range(-5.0..5.0),
             },
-            SpriteBundle {
+            SpriteSheetBundle {
                 transform: Transform {
-                    translation: Vec3::new(rng.gen_range(WINDOW_LEFT_X..WINDOW_RIGHT_X) as f32,
-                                           rng.gen_range(WINDOW_BOTTOM_Y..WINDOW_TOP_Y) as f32,
-                                           0.1),
-                    scale: Vec3::new(0.1, 0.1, 1.0),
+                    translation: Vec3::new(
+                        rng.gen_range(WINDOW_LEFT_X..WINDOW_RIGHT_X) as f32,
+                        rng.gen_range(WINDOW_BOTTOM_Y..WINDOW_TOP_Y) as f32,
+                        0.1,
+                    ),
                     ..Default::default()
                 },
-                texture: asset_server.load("red.png"),
+                texture_atlas: texture_atlas_handle.clone(),
+                sprite: TextureAtlasSprite { index: i, ..default() },
                 ..Default::default()
             },
         ));
     }
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle { // Background
+}
+
+fn setup_background(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(SpriteBundle {
+        // Background
         texture: asset_server.load("dark.png"),
         transform: Transform::from_scale(Vec3::new(1.0, 1.0, -1.0)),
         ..Default::default()
@@ -70,62 +95,87 @@ fn update_fish(mut query: Query<(&MobileFish, &mut Direction, &mut Transform)>) 
     let mut rng = rand::thread_rng();
 
     for (fish, mut fish_direction, mut fish_transform) in query.iter_mut() {
-        info!("update_fish 🐟{}({}) h{} v{}", fish.name, fish_transform.translation, fish_direction.horizontal_speed, fish_direction.vertical_speed);
+        debug!(
+            "update_fish 🐟{}({}) h{} v{}",
+            fish.name,
+            fish_transform.translation,
+            fish_direction.horizontal_speed,
+            fish_direction.vertical_speed
+        );
         fish_direction.horizontal_speed += rng.gen_range(-0.1..0.1);
         fish_direction.vertical_speed += rng.gen_range(-0.1..0.1);
+
+        if fish_direction.horizontal_speed > 0.0 {
+            fish_transform.scale = Vec3::new(1.0, 1.0, 1.0);
+        } else if fish_direction.horizontal_speed < 0.0 {
+            fish_transform.scale = Vec3::new(-1.0, 1.0, 1.0);
+        }
     }
 }
 
 fn move_fish(mut query: Query<(&MobileFish, &mut Direction, &mut Transform)>) {
     for (fish, mut fish_direction, mut fish_transform) in query.iter_mut() {
-        debug!("move_fish 🐟{}({}) h{} v{}", fish.name, fish_transform.translation, fish_direction.horizontal_speed, fish_direction.vertical_speed);
+        debug!(
+            "move_fish 🐟{}({}) h{} v{}",
+            fish.name,
+            fish_transform.translation,
+            fish_direction.horizontal_speed,
+            fish_direction.vertical_speed
+        );
         fish_transform.translation.x += fish_direction.horizontal_speed;
         fish_transform.translation.y += fish_direction.vertical_speed;
 
-        if (fish_transform.translation.x > WINDOW_RIGHT_X as f32) ||
-            (fish_transform.translation.x < WINDOW_LEFT_X as f32)
+        if (fish_transform.translation.x > WINDOW_RIGHT_X as f32)
+            || (fish_transform.translation.x < WINDOW_LEFT_X as f32)
         {
             fish_direction.horizontal_speed *= -1.0;
         }
 
-        if (fish_transform.translation.y > WINDOW_TOP_Y as f32) ||
-            (fish_transform.translation.y < WINDOW_BOTTOM_Y as f32)
+        if (fish_transform.translation.y > WINDOW_TOP_Y as f32)
+            || (fish_transform.translation.y < WINDOW_BOTTOM_Y as f32)
         {
             fish_direction.vertical_speed *= -1.0;
         }
     }
 }
 
+fn tick_fish() {
+    info!("Tick!");
+}
+
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::MIDNIGHT_BLUE)) // background color
-
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            // set up window
-            primary_window: Some(Window {
-                fit_canvas_to_parent: true, // fill the entire browser window
-                resolution: WindowResolution::new(WINDOW_WIDTH as f32, WINDOW_HEIGHT as f32),
-                resizable: false,
-                prevent_default_event_handling: false, // don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
-                title: "Fishtank! - ajw@ajw.io".to_string(),
-                ..default()
-            }),
-            ..default()
-        }))
-
-        .add_startup_systems((setup, spawn_fish))
-
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest()) // avoid blurry sprites
+                .set(WindowPlugin {
+                    // set up window
+                    primary_window: Some(Window {
+                        fit_canvas_to_parent: true, // fill the entire browser window
+                        resolution: WindowResolution::new(
+                            WINDOW_WIDTH as f32,
+                            WINDOW_HEIGHT as f32,
+                        ),
+                        resizable: false,
+                        prevent_default_event_handling: false, // don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
+                        title: "Fishtank! - ajw@ajw.io".to_string(),
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        )
+        .add_startup_systems((setup_camera, setup_background, spawn_fish))
         // SystemInformationDiagnostics don't work if you're dynamic linking. :|
         .add_plugin(SystemInformationDiagnosticsPlugin::default())
         .add_plugin(LogDiagnosticsPlugin::default())
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
-
         .add_system(move_fish)
         .add_system(update_fish)
-        //.add_system(
-        //    update_fish
-        //        .in_schedule(CoreSchedule::FixedUpdate)
-        //        .run_if(on_fixed_timer(Duration::from_secs(TIMESTEP_1_PER_SECOND))),
-        //)
+        .add_system(
+            tick_fish
+                .in_schedule(CoreSchedule::FixedUpdate)
+                .run_if(on_fixed_timer(Duration::from_secs(TIMESTEP_1_PER_SECOND))),
+        )
         .run();
 }
